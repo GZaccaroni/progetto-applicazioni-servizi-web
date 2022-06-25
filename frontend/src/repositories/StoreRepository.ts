158d1266e1ca81852727639ed6cccd28c8b8b6ad
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-06-25 19:40:17 +0200

    Implemented StoreRepository

---
+++ frontend/src/repositories/StoreRepository.ts
@@ -0,0 +1,46 @@
+import Client from "@/repositories/common/AxiosClient";
+import { DbItem } from "@/model/DbItem";
+import { PaginatedResult } from "@/repositories/common/PaginatedResult";
+import { omit } from "lodash";
+import { DbCustomer } from "@/model/DbCustomer";
+import { DbStore } from "@/model/DbStore";
+
+const resource = "/store";
+
+export interface FindStoresInput {
+  authorized?: boolean;
+  pagingNext?: string;
+  pagingPrevious?: string;
+  searchName?: string;
+  limit: number;
+}
+export async function findStores(
+  input: FindStoresInput
+): Promise<PaginatedResult<DbStore[]>> {
+  const result = await Client.get<PaginatedResult<DbStore[]>>(
+    `${resource}/find`,
+    {
+      params: input,
+    }
+  );
+  return result.data;
+}
+export async function findStore(id: string): Promise<DbStore> {
+  const result = await Client.get<DbStore>(`${resource}/${id}`);
+  return result.data;
+}
+export async function addStore(data: Omit<DbStore, "id">): Promise<void> {
+  const result = await Client.post<void>(`${resource}`, data);
+  return result.data;
+}
+export async function updateStore(item: DbStore): Promise<void> {
+  const result = await Client.post<void>(
+    `${resource}/${item.id}`,
+    omit(item, "id")
+  );
+  return result.data;
+}
+export async function deleteStore(id: string): Promise<void> {
+  const result = await Client.delete<void>(`${resource}/${id}`);
+  return result.data;
+}
