4b2f2b8a351e798172590b5b8ddcb9bbbfe7e745
alexbaiardi <alex.baiardi99@gmail.com>
2022-06-28 10:31:10 +0200

    Add Orders request validation

--- packages/backend/src/controllers/OrderController.ts
+++ packages/backend/src/controllers/OrderController.ts
@@ -1,7 +1,14 @@
 import {Request, Response} from "express";
+import {validateRequest} from "../schema/validation";
+import {CreateOrder} from "../schema/type/CreateOrder";
+import {UpdateOrder} from "../schema/type/UpdateOrder";
 
 export const addOrder=(req:Request,res: Response)=>{
-  res.send("Add Order")
+  if(!validateRequest<CreateOrder>("CreateOrder",req.body)){
+    res.status(400).send("Invalid Input");
+    return;
+  }
+  res.send("Add Order valid")
 }
 export const getOrders=(req:Request,res: Response)=>{
   res.send("Get Orders")
@@ -10,7 +17,11 @@ export const getOrderById=(req:Request,res: Response)=>{
   res.send("GetId Order")
 }
 export const updateOrder=(req:Request,res: Response)=>{
-  res.send("Update Order")
+  if(!validateRequest<UpdateOrder>("UpdateOrder",req.body)){
+    res.status(400).send("Invalid Input");
+    return;
+  }
+  res.send("Update Order valid")
 }
 export const deleteOrder=(req:Request,res: Response)=>{
   res.send("Add Order")
--- packages/backend/src/schema/validation.ts
+++ packages/backend/src/schema/validation.ts
@@ -6,6 +6,9 @@ import Product from "./json/Product.json";
 import ProductKind from "./json/ProductKind.json";
 import CreateProduct from "./json/CreateProduct.json"
 import UpdateProduct from "./json/UpdateProduct.json";
+import CreateOrderEntry from "./json/CreateOrderEntry.json";
+import CreateOrder from "./json/CreateOrder.json";
+import UpdateOrder from "./json/UpdateOrder.json";
 
 export const ajv=new Ajv();
 addFormats(ajv)
@@ -15,6 +18,9 @@ ajv.addSchema(ProductKind,"ProductKind");
 ajv.addSchema(Product, "Product");
 ajv.addSchema(CreateProduct,"CreateProduct")
 ajv.addSchema(UpdateProduct, "UpdateProduct");
+ajv.addSchema(CreateOrderEntry, "CreateOrderEntry");
+ajv.addSchema(CreateOrder,"CreateOrder");
+ajv.addSchema(UpdateOrder, "UpdateOrder");
 
 export function validateRequest<T>(key:string, reqBody:string):Promise<T> | boolean{
   const validate= ajv.getSchema<T>(key);
