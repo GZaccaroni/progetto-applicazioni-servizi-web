2eeb210f18d631261ff704ca9bac3e7e4a650238
alexbaiardi <alex.baiardi99@gmail.com>
2022-06-28 10:50:42 +0200

    Add Customer request validation

--- packages/backend/src/controllers/CustomerController.ts
+++ packages/backend/src/controllers/CustomerController.ts
@@ -1,6 +1,13 @@
 import {Request, Response} from "express";
+import {validateRequest} from "../schema/validation";
+import {CreateCustomer} from "../schema/type/CreateCustomer";
+import {UpdateCustomer} from "../schema/type/UpdateCustomer";
 
 export const addCustomer=(req:Request,res: Response)=>{
+  if(!validateRequest<CreateCustomer>("CreateOrder",req.body)){
+    res.status(400).send("Invalid Input");
+    return;
+  }
   res.send("Add Customer")
 }
 export const getCustomers=(req:Request,res: Response)=>{
@@ -10,7 +17,11 @@ export const getCustomerById=(req:Request,res: Response)=>{
   res.send("GetId Customer")
 }
 export const updateCustomer=(req:Request,res: Response)=>{
-  res.send("Update Customer")
+  if(!validateRequest<UpdateCustomer>("UpdateOrder",req.body)){
+    res.status(400).send("Invalid Input");
+    return;
+  }
+  res.send("Update Customer valid")
 }
 export const deleteCustomer=(req:Request,res: Response)=>{
   res.send("Add Customer")
--- packages/backend/src/schema/validation.ts
+++ packages/backend/src/schema/validation.ts
@@ -9,6 +9,9 @@ import UpdateProduct from "./json/UpdateProduct.json";
 import CreateOrderEntry from "./json/CreateOrderEntry.json";
 import CreateOrder from "./json/CreateOrder.json";
 import UpdateOrder from "./json/UpdateOrder.json";
+import Customer from "./json/Customer.json";
+import CreateCustomer from "./json/CreateCustomer.json";
+import UpdateCustomer from "./json/UpdateCustomer.json";
 
 export const ajv=new Ajv();
 addFormats(ajv)
@@ -21,6 +24,9 @@ ajv.addSchema(UpdateProduct, "UpdateProduct");
 ajv.addSchema(CreateOrderEntry, "CreateOrderEntry");
 ajv.addSchema(CreateOrder,"CreateOrder");
 ajv.addSchema(UpdateOrder, "UpdateOrder");
+ajv.addSchema(Customer,"Customer");
+ajv.addSchema(CreateCustomer,"CreateCustomer");
+ajv.addSchema(UpdateCustomer,"UpdateCustomer");
 
 export function validateRequest<T>(key:string, reqBody:string):Promise<T> | boolean{
   const validate= ajv.getSchema<T>(key);
