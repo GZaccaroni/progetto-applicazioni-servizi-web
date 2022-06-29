af005db678e5febce7a8e5e6928b24a0cf36c856
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-06-29 15:50:43 +0200

    Added Customers List

---
+++ packages/frontend/src/components/customers/ListCustomers.vue
@@ -0,0 +1,84 @@
+<template>
+  <div style="width: 100%">
+    <paginated-table-builder
+      :observe-items-fn="observeItemsFn"
+      :observe-fn-input="observeFnInput"
+      @onRowEvent="onRowEvent"
+      :columns="headers"
+      :actions="['edit', 'delete']"
+      :loading="tableLoading"
+    >
+      <template v-slot:header>
+        <list-customers-filter @change="filterList" />
+      </template>
+    </paginated-table-builder>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "@vue/composition-api";
+import { DataTableHeader } from "vuetify";
+import { TableItemEvent } from "@/plugins/table-builder/TableItemEventType";
+import i18n from "@/i18n";
+import { FindStoresInput } from "@/repositories/StoreRepository";
+import { DbStore } from "@/model/db/DbStore";
+import {
+  FindCustomersInput,
+  observeCustomers,
+} from "@/repositories/CustomerRepository";
+import ListCustomersFilter from "@/components/customers/ListCustomersFilter.vue";
+
+export default defineComponent({
+  components: {
+    ListCustomersFilter,
+  },
+  setup() {
+    const observeItemsFn = observeCustomers;
+    const tableLoading = ref(false);
+    const observeFnInput = ref<FindCustomersInput>({ limit: 10 });
+    const headers = ref<DataTableHeader[]>([
+      {
+        text: i18n.t("model.customer.name").toString(),
+        value: "name",
+        sortable: false,
+      },
+      {
+        text: i18n.t("model.address.name").toString(),
+        value: "address",
+        sortable: false,
+      },
+      {
+        text: i18n.t("model.phoneNumber.name").toString(),
+        value: "phoneNumber",
+        sortable: false,
+      },
+      {
+        text: i18n.t("model.vatNumber.name").toString(),
+        value: "vatNumber",
+        sortable: false,
+      },
+      {
+        text: i18n.t("word.actions").toString(),
+        value: "actions",
+        sortable: false,
+        width: "130px",
+      },
+    ]);
+    return {
+      observeItemsFn,
+      observeFnInput,
+      tableLoading,
+      headers,
+    };
+  },
+  methods: {
+    filterList(input: FindStoresInput) {
+      this.observeFnInput = input;
+    },
+    onRowEvent(event: TableItemEvent<DbStore>) {
+      // Pass-through
+      this.$emit("onRowEvent", event);
+    },
+  },
+});
+</script>
---
+++ packages/frontend/src/components/customers/ListCustomersFilter.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-form class="pa-4" @submit.prevent>
+    <v-text-field
+      v-model="form.searchName"
+      :label="$t('components.ListStoresFilter.searchPerName')"
+    ></v-text-field>
+  </v-form>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, watch } from "@vue/composition-api";
+import { removeBlanks } from "@/helpers/utils";
+import { FindCustomersInput } from "@/repositories/CustomerRepository";
+
+export default defineComponent({
+  setup(props, { emit }) {
+    const form = reactive<FindCustomersInput>({
+      limit: 10,
+    });
+    watch(form, (newValue) => {
+      emit("change", removeBlanks(newValue));
+    });
+    return { form };
+  },
+});
+</script>
