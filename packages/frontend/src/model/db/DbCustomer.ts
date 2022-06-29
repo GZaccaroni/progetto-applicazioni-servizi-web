f6926f4057aa8b252becf0fc56ae226ad62c8555
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-06-29 08:43:28 +0200

    Addded address to customer

--- packages/frontend/src/model/db/DbCustomer.ts
+++ packages/frontend/src/model/db/DbCustomer.ts
@@ -2,6 +2,7 @@ import { DbIdentifiable } from "./DbIdentifiable";
 
 export interface DbCustomer extends DbIdentifiable {
   name: string;
+  address?: string;
   phoneNumber?: string;
   vatNumber?: string;
 }
