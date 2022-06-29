7fa969e5c2d5ea9ad1c112a433b4cafa0ef7b371
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-06-29 10:04:50 +0200

    Added ConfirmDialog

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -6,11 +6,13 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import PaginatedTableBuilder from "@/plugins/paginated-table-builder/main";
 import TableBuilder from "@/plugins/table-builder/main";
+import ConfirmDialog from "@/plugins/confirm-dialog/main";
 
 Vue.config.productionTip = false;
 Vue.use(VueCompositionAPI);
 Vue.use(PaginatedTableBuilder);
 Vue.use(TableBuilder);
+Vue.use(ConfirmDialog);
 
 new Vue({
   router,
