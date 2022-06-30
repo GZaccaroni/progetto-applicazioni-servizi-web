9d155974bd7ec83a0c4873812aa71b2753efdb78
alexbaiardi <alex.baiardi99@gmail.com>
2022-06-30 17:33:25 +0200

    Update user schema db

--- packages/backend/src/model/db_schema/UserSchema.ts
+++ packages/backend/src/model/db_schema/UserSchema.ts
@@ -1,7 +1,11 @@
-// Create Model
-import mongoose from "mongoose";
+import mongoose, {PassportLocalSchema} from "mongoose";
+import passportLocalMongoose from "passport-local-mongoose";
 
 export const User = new mongoose.Schema({
   username: String,
   isAdmin: Boolean
 });
+
+User.plugin(passportLocalMongoose);
+
+export default mongoose.model('userData', User as PassportLocalSchema<any, any>, "users");
