4b6230dbdcf017dfdba315a4dcca9237a79b1a2b
alexbaiardi <alex.baiardi99@gmail.com>
2022-07-04 15:27:13 +0200

    Implemented base logic

--- packages/backend/src/controllers/StoreController.ts
+++ packages/backend/src/controllers/StoreController.ts
@@ -2,24 +2,91 @@ import {Request, Response} from "express";
 import {validateRequest} from "../model/request/validation";
 import {CreateStore} from "../model/request/type/CreateStore";
 import {UpdateStore} from "../model/request/type/UpdateStore";
+import Store from "../model/db_model/Store";
+import Log from "../model/db_model/Log";
+import {paginateOptions, paginateResponse} from "../paginationUtils";
 
-export const addStore=(req:Request,res: Response)=>{
+export const addStore=(req,res: Response)=>{
+/*  if(!req.user.isAdmin){
+    res.status(403).json({message:"User not authorized"});
+  }*/
   if(!validateRequest<CreateStore>("CreateStore",req.body)){
-    res.status(400).send("Invalid Input");
+    res.status(400).json({ message:"Invalid Input"});
     return;
   }
-  res.send("Add Store valid")
+  Store.create(req.body).then(
+    store=>{
+      Log.create({
+        username: req.user.username,
+        action: "Create",
+        objectId: store._id
+      }).then(() => res.json("Add Store"));
+    });
+
 }
-export const getStores=(req:Request,res: Response)=>{
-  res.send("Get Stores")
+export const getStores=(req,res: Response)=>{
+  if (!req.query.limit) {
+    res.status(400).json({message: "Bad request"});
+    return;
+  }
+  const query={};
+  if(req.query.authorized){
+    query["authorizations"]={ userId: req.user.id}
+  }
+  if (req.query.searchName) {
+    query["name"] = {$regex: req.query.searchName};
+  }
+  const options = paginateOptions(query,
+                                  ["name", "authorizations", "_id"],
+                                    req.query.limit,
+                                    req.query.pagingNext,
+                                    req.query.paginatePrevious);
+  Store.paginate(options, err => res.json(err)).then((result) => {
+    res.json(paginateResponse(result));
+  });
 }
 export const getStoreById=(req:Request,res: Response)=>{
-  res.send("GetId Store")
-}
-export const updateStore=(req:Request,res: Response)=>{
-  if(!validateRequest<UpdateStore>("UpdateStore",req.body)){
-    res.status(400).send("Invalid Input");
+
+  if (!req.params.storeId) {
+    res.status(400).send({message: "Invalid ID supplied"});
     return;
   }
-  res.send("Update Store valid")
+  //TODO Not authorized
+  Store.findOne({storeId: req.params.storeId}, ["name", "authorizations", "_id"], (err, store) => {
+    if (err) {
+      res.json(err);
+    } else {
+      if (store == null) {
+        res.status(404).json({message: "Store not found"});
+      } else {
+        res.json(store);
+      }
+    }
+  });
+}
+export const updateStore = (req, res: Response) => {
+  /*  if(!req.user.isAdmin){
+    res.status(403).json({message:"User not authorized"});
+  }*/
+  if (!validateRequest<UpdateStore>("UpdateStore", req.body) || !req.params.storeId) {
+    res.status(400).json({message: "Invalid Input"});
+    return;
+  }
+  Store.findByIdAndUpdate(req.params.storeId, req.body, {new: true}, (err, store) => {
+    if (err)
+      res.json(err);
+    else {
+      if (store == null) {
+        res.status(404).send({
+          message: 'Store not found'
+        });
+      } else {
+        Log.create({
+          username: req.user.username,
+          action: "Update",
+          objectId: store._id
+        }).then(() => res.json(store), (err) => res.json(err));
+      }
+    }
+  });
 }
