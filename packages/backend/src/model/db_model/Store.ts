1534bb3c4179eefac3bafc35a8f8da654cfea997
alexbaiardi <alex.baiardi99@gmail.com>
2022-07-08 12:24:03 +0200

    Add predefined projection

--- packages/backend/src/model/db_model/Store.ts
+++ packages/backend/src/model/db_model/Store.ts
@@ -14,3 +14,13 @@ const StoreSchema = new mongoose.Schema({
 StoreSchema.plugin(mongoosePagination);
 
 export default mongoose.model<StoreDocument, Pagination<StoreDocument>>('store', StoreSchema, 'stores');
+
+export const StoreProjection = {
+  _id: 0,
+  id: '$_id',
+  name: 1,
+  authorizations: {
+    userId: 1,
+    accessLevel: 1
+  }
+}
