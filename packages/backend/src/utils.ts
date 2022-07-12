6b1b024a62f728e6b15270d24cca6baa030850df
alexbaiardi <alex.baiardi99@gmail.com>
2022-07-12 15:13:19 +0200

    Delete debug log

--- packages/backend/src/utils.ts
+++ packages/backend/src/utils.ts
@@ -1,6 +1,4 @@
 export const isUserLoggedIn = (req, res, next) => {
-  console.log(req.user);
-  console.log(req.isAuthenticated());
   if (!req.isAuthenticated()) {
     res.status(401).send("User not authenticated");
     return;
