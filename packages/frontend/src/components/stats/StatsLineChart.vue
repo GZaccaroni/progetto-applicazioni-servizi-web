41fde4dd5b053f67f14d4e6f59606de859b4a738
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-08-10 19:50:54 +0200

    Implemented Line Chart

---
+++ packages/frontend/src/components/stats/StatsLineChart.vue
@@ -0,0 +1,92 @@
+<template>
+  <Line
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    dataset-id-key="label"
+    :height="height"
+  />
+</template>
+<script setup lang="ts">
+import { Line } from "vue-chartjs/legacy";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  ChartDataset,
+} from "chart.js";
+import { computed, defineProps, PropType } from "vue";
+import { DbChartData } from "@/model/db/DbChartData";
+import { stringToColor } from "@/helpers/utils";
+import { groupBy, sumBy } from "lodash";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+const props = defineProps({
+  height: {
+    type: Number,
+    default: 400,
+  },
+  data: {
+    type: Object as PropType<DbChartData>,
+    required: true,
+  },
+  global: {
+    type: Boolean,
+  },
+});
+const chartOptions = {
+  responsive: true,
+};
+const chartData = computed(() => {
+  let datasets: ChartDataset[];
+  if (props.global) {
+    datasets = [getGlobalDataset()];
+  } else {
+    datasets = getAtomicDataset();
+  }
+  return {
+    labels: getLabels(),
+    datasets: datasets,
+  };
+});
+function getLabels(): string[] {
+  return props.data.data.flatMap((el) =>
+    el.productData.map((el) => dateToString(el.date))
+  );
+}
+function getAtomicDataset(): ChartDataset[] {
+  return props.data.data.map((el) => {
+    return {
+      label: el.productName,
+      backgroundColor: stringToColor(el.productName),
+      data: el.productData.map((el) => el.value),
+    };
+  });
+}
+function getGlobalDataset(): ChartDataset {
+  const datas = props.data.data.flatMap((entry) => entry.productData);
+  const groupedData = groupBy(datas, "date");
+  const values = Object.values(groupedData).map((data) => {
+    return sumBy(data, "value");
+  });
+  return {
+    label: "",
+    backgroundColor: "#000000",
+    data: values,
+  };
+}
+
+function dateToString(date: Date): string {
+  return date.toLocaleDateString();
+}
+</script>
