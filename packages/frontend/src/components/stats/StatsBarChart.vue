ac0ca7ca10b91472b65ab5a5689befefff781cbc
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-08-10 19:50:47 +0200

    Implemented Bar Chart

---
+++ packages/frontend/src/components/stats/StatsBarChart.vue
@@ -0,0 +1,57 @@
+<template>
+  <Bar
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    dataset-id-key="label"
+    :height="height"
+  />
+</template>
+<script setup lang="ts">
+import { Bar } from "vue-chartjs/legacy";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+import { computed, defineProps, PropType } from "vue";
+import { DbChartData } from "@/model/db/DbChartData";
+import { stringToColor } from "@/helpers/utils";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+const props = defineProps({
+  height: {
+    type: Number,
+    default: 400,
+  },
+  data: {
+    type: Object as PropType<DbChartData>,
+    required: true,
+  },
+});
+const chartOptions = {
+  responsive: true,
+};
+const chartData = computed(() => {
+  return {
+    labels: [""],
+    datasets: props.data.data.map((el) => {
+      return {
+        label: el.productName,
+        backgroundColor: stringToColor(el.productName),
+        data: [el.value],
+      };
+    }),
+  };
+});
+</script>
