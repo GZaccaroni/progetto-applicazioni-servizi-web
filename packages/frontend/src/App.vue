b606feaeed3660c18ea01ca847f6dc591c2c364d
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-08-13 15:48:14 +0200

    Added dynamic lang to html tag

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -15,6 +15,7 @@ import AppSnackbar from "@/components/common/AppSnackbar.vue";
 import { defineComponent } from "vue";
 import AppNavbar from "@/components/common/AppNavbar.vue";
 import { mapGetters } from "vuex";
+import { setDocumentLang } from "@/i18n";
 
 export default defineComponent({
   name: "App",
@@ -31,5 +32,17 @@ export default defineComponent({
   data: () => ({
     loginDialogVisible: false,
   }),
+  mounted() {
+    this.$watch(
+      "$i18n.locale",
+      (newLocale, oldLocale) => {
+        if (newLocale === oldLocale) {
+          return;
+        }
+        setDocumentLang(newLocale);
+      },
+      { immediate: true }
+    );
+  },
 });
 </script>
