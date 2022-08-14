606f213db177078641e7259fdaaea5ebf01bacf0
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-08-14 08:21:33 +0200

    Refactor: Migrated users list to Vue Composition

--- packages/frontend/src/components/users/ListUsers.vue
+++ packages/frontend/src/components/users/ListUsers.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="width: 100%">
     <paginated-table-builder
-      :observe-items-fn="observeItemsFn"
+      :observe-items-fn="observeUsers"
       :observe-fn-input="observeFnInput"
       @onRowEvent="onRowEvent"
       :columns="headers"
@@ -21,8 +21,8 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from "vue";
+<script setup lang="ts">
+import { ref } from "vue";
 import { FindUsersInput, observeUsers } from "@/repositories/UserRepository";
 import { DataTableHeader } from "vuetify";
 import { DbUser } from "@/model/db/DbUser";
@@ -30,48 +30,35 @@ import { TableItemEvent } from "@/plugins/table-builder/TableItemEventType";
 import ListUsersFilter from "@/components/users/ListUsersFilter.vue";
 import i18n from "@/i18n";
 
-export default defineComponent({
-  components: {
-    ListUsersFilter,
+const emit = defineEmits(["onRowEvent"]);
+
+const tableLoading = ref(false);
+const observeFnInput = ref<FindUsersInput>({ limit: 10 });
+const headers = ref<DataTableHeader[]>([
+  {
+    text: i18n.t("model.user.username").toString(),
+    value: `username`,
+    sortable: false,
   },
-  setup() {
-    const observeItemsFn = observeUsers;
-    const tableLoading = ref(false);
-    const observeFnInput = ref<FindUsersInput>({ limit: 10 });
-    const headers = ref<DataTableHeader[]>([
-      {
-        text: i18n.t("model.user.username").toString(),
-        value: `username`,
-        sortable: false,
-      },
-      {
-        text: i18n.t("model.user.isAdmin").toString(),
-        value: "isAdmin",
-        sortable: false,
-        width: "130px",
-      },
-      {
-        text: i18n.t("word.actions").toString(),
-        value: "actions",
-        sortable: false,
-        width: "130px",
-      },
-    ]);
-    return {
-      observeItemsFn,
-      observeFnInput,
-      tableLoading,
-      headers,
-    };
+  {
+    text: i18n.t("model.user.isAdmin").toString(),
+    value: "isAdmin",
+    sortable: false,
+    width: "130px",
   },
-  methods: {
-    filterList(input: FindUsersInput) {
-      this.observeFnInput = input;
-    },
-    onRowEvent(event: TableItemEvent<DbUser>) {
-      // Pass-through
-      this.$emit("onRowEvent", event);
-    },
+  {
+    text: i18n.t("word.actions").toString(),
+    value: "actions",
+    sortable: false,
+    width: "130px",
   },
-});
+]);
+
+function filterList(input: FindUsersInput) {
+  observeFnInput.value = input;
+}
+function onRowEvent(event: TableItemEvent<DbUser>) {
+  // Pass-through
+  emit("onRowEvent", event);
+}
 </script>
