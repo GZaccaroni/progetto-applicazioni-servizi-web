71219378514e8d77e9c0795ee99363290e2ba22a
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-08-14 15:23:02 +0200

    Await logout before going to homepage

--- packages/frontend/src/helpers/errorHandler.ts
+++ packages/frontend/src/helpers/errorHandler.ts
@@ -4,9 +4,8 @@ import router from "@/router";
 import i18n from "@/i18n";
 import { useUserStore } from "@/store/user";
 
-export function repositoryErrorHandler(reason: unknown, isLogin = false) {
+export async function repositoryErrorHandler(reason: unknown, isLogin = false) {
   let errorMessage: string;
-  console.error("Repository error", reason);
   if (reason instanceof AxiosError) {
     switch (reason.response?.status) {
       case 400:
@@ -20,8 +19,12 @@ export function repositoryErrorHandler(reason: unknown, isLogin = false) {
         errorMessage = i18n.t("error.itemNotFound").toString();
         break;
       case 401:
-        useUserStore().logout();
-        router.push("/");
+        try {
+          await useUserStore().logout();
+          await router.push("/");
+        } catch {
+          // Ignore errors
+        }
         errorMessage = i18n.t("error.userNotLoggedIn").toString();
         break;
       case 403:
