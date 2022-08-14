0e1b523cc20f7665306ad55895fd7f44cff16506
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-08-14 08:17:14 +0200

    Refactor: Migrated orders list to Vue Composition

--- packages/frontend/src/components/orders/ListOrders.vue
+++ packages/frontend/src/components/orders/ListOrders.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="width: 100%">
     <paginated-table-builder
-      :observe-items-fn="observeItemsFn"
+      :observe-items-fn="observeOrders"
       :observe-fn-input="observeFnInput"
       @onRowEvent="onRowEvent"
       :columns="headers"
@@ -28,70 +28,55 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from "vue";
+<script setup lang="ts">
+import { ref } from "vue";
 import { DataTableHeader } from "vuetify";
 import { TableItemEvent } from "@/plugins/table-builder/TableItemEventType";
 import i18n from "@/i18n";
 import { FindOrdersInput, observeOrders } from "@/repositories/OrderRepository";
-import { FindStoresInput } from "@/repositories/StoreRepository";
 import ListOrdersFilter from "@/components/orders/ListOrdersFilter.vue";
 import { DbOrder } from "@/model/db/DbOrder";
 import ListOrderEntries from "@/components/orders/ListOrderEntries.vue";
 
-export default defineComponent({
-  components: {
-    ListOrderEntries,
-    ListOrdersFilter,
+const emit = defineEmits(["onRowEvent"]);
+
+const tableLoading = ref(false);
+const observeFnInput = ref<FindOrdersInput>({ limit: 10 });
+const headers = ref<DataTableHeader[]>([
+  {
+    text: i18n.t("model.order.date").toString(),
+    value: "date",
+    sortable: false,
   },
-  setup() {
-    const observeItemsFn = observeOrders;
-    const tableLoading = ref(false);
-    const observeFnInput = ref<FindOrdersInput>({ limit: 10 });
-    const headers = ref<DataTableHeader[]>([
-      {
-        text: i18n.t("model.order.date").toString(),
-        value: "date",
-        sortable: false,
-      },
-      {
-        text: i18n.t("model.order.store").toString(),
-        value: "store.name",
-        sortable: false,
-      },
-      {
-        text: i18n.t("model.order.customer").toString(),
-        value: "customer.name",
-        sortable: false,
-      },
-      {
-        text: i18n.t("model.order.price").toString(),
-        value: "price",
-        sortable: false,
-      },
-      { text: "", value: "data-table-expand" },
-      {
-        text: i18n.t("word.actions").toString(),
-        value: "actions",
-        sortable: false,
-        width: "130px",
-      },
-    ]);
-    return {
-      observeItemsFn,
-      observeFnInput,
-      tableLoading,
-      headers,
-    };
+  {
+    text: i18n.t("model.order.store").toString(),
+    value: "store.name",
+    sortable: false,
   },
-  methods: {
-    filterList(input: FindStoresInput) {
-      this.observeFnInput = input;
-    },
-    onRowEvent(event: TableItemEvent<DbOrder>) {
-      // Pass-through
-      this.$emit("onRowEvent", event);
-    },
+  {
+    text: i18n.t("model.order.customer").toString(),
+    value: "customer.name",
+    sortable: false,
   },
-});
+  {
+    text: i18n.t("model.order.price").toString(),
+    value: "price",
+    sortable: false,
+  },
+  { text: "", value: "data-table-expand" },
+  {
+    text: i18n.t("word.actions").toString(),
+    value: "actions",
+    sortable: false,
+    width: "130px",
+  },
+]);
+
+function filterList(input: FindOrdersInput) {
+  observeFnInput.value = input;
+}
+function onRowEvent(event: TableItemEvent<DbOrder>) {
+  // Pass-through
+  emit("onRowEvent", event);
+}
 </script>
