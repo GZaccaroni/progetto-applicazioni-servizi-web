a916f03f21136b7a78d26cf8152bdb78a6afa95e
Giulio Zaccaroni <zaccaronigiulio@gmail.com>
2022-09-04 07:45:34 +0200

    Redacted secrets

--- apps/backend/src/config.ts
+++ apps/backend/src/config.ts
@@ -1,7 +1,7 @@
 export const MONGODB_CONNECTION_STRING =
-  process.env.MONGODB_CONNECTION_STRING ||
-  "mongodb+srv://admin:C0W6J0tA3jl8X4Cb@maincolturecloud.fujik.mongodb.net/main";
+  process.env.MONGODB_CONNECTION_STRING || "##changewithdevelopment_server##";
 export const SESSIONS_COLLECTION = "sessions";
 export const SESSION_MAX_AGE = 60 * 60 * 1000; // 1 hour;
+// The following secret should be changed for every install
 export const SESSION_SECRET =
   process.env.SESSION_SECRET || "r8q,+&1LM3)CD*zAGpx1xm{Pusadnstrc;#";
